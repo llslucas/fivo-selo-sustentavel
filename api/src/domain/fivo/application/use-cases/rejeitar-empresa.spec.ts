2b93fc403b95ad7c1da57e529a1a0dc7809e2593
llslucas <lucas.souza473@outlook.com>
2026-09-02 20:12:22 -0300

    feat: create `rejeitar-empresa` use-case

---
+++ api/src/domain/fivo/application/use-cases/rejeitar-empresa.spec.ts
@@ -0,0 +1,54 @@
+import { NotAllowedError } from '@core/errors/not-allowed-error';
+import { ResourceNotFoundError } from '@core/errors/resource-not-found-error';
+import { UserType } from '@domain/fivo/entities/user';
+import { EmpresaFactory } from '@test/factories/empresa-factory';
+import { UserFactory } from '@test/factories/user-factory';
+import { InMemoryEmpresaRepository } from '@test/repositories/in-memory-empresa-repository';
+import { EmpresaRepository } from '../ports/database/empresa-repository';
+import { RejeitarEmpresaUseCase } from './rejeitar-empresa';
+import { EmpresaStatus } from '@domain/fivo/entities/empresa';
+
+describe('RejeitarEmpresaUseCase', () => {
+  let rejeitarEmpresaUseCase: RejeitarEmpresaUseCase;
+  let empresaRepository: EmpresaRepository;
+
+  beforeEach(() => {
+    empresaRepository = new InMemoryEmpresaRepository();
+    rejeitarEmpresaUseCase = new RejeitarEmpresaUseCase(empresaRepository);
+  });
+
+  it('should reject an existing empresa if the user is an admin', async () => {
+    const mockUser = UserFactory.create({ type: UserType.ADMIN });
+
+    const mockEmpresa = EmpresaFactory.create();
+    await empresaRepository.create(mockEmpresa);
+
+    await rejeitarEmpresaUseCase.execute(mockEmpresa.id.toString(), mockUser);
+
+    const updatedEmpresa = await empresaRepository.findById(
+      mockEmpresa.id.toString(),
+    );
+
+    expect(updatedEmpresa?.status).toBe(EmpresaStatus.REJEITADA);
+    expect(updatedEmpresa?.decidido_por).toEqual(mockUser);
+  });
+
+  it('should throw a NotAllowedError if the user is not an admin', async () => {
+    const mockUser = UserFactory.create({ type: UserType.EMPRESA });
+
+    const mockEmpresa = EmpresaFactory.create();
+    await empresaRepository.create(mockEmpresa);
+
+    await expect(
+      rejeitarEmpresaUseCase.execute(mockEmpresa.id.toString(), mockUser),
+    ).rejects.toThrow(NotAllowedError);
+  });
+
+  it('should throw a ResourceNotFound Error if the empresa not exists.', async () => {
+    const mockUser = UserFactory.create({ type: UserType.ADMIN });
+
+    await expect(
+      rejeitarEmpresaUseCase.execute('non-existent-id', mockUser),
+    ).rejects.toThrow(ResourceNotFoundError);
+  });
+});
