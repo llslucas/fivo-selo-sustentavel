7450178e93097f4722d8c64e652e0a8f7f5c3648
llslucas <lucas.souza473@outlook.com>
2026-09-02 20:37:02 -0300

    feat: create `suspender-empresa` use case

---
+++ api/src/domain/fivo/application/use-cases/suspender-empresa.ts
@@ -0,0 +1,27 @@
+import { User, UserType } from '@domain/fivo/entities/user';
+import { EmpresaRepository } from '../ports/database/empresa-repository';
+import { NotAllowedError } from '@core/errors/not-allowed-error';
+import { ResourceNotFoundError } from '@core/errors/resource-not-found-error';
+import { EmpresaStatus } from '@domain/fivo/entities/empresa';
+
+export class SuspenderEmpresaUseCase {
+  constructor(private readonly empresaRepository: EmpresaRepository) {}
+
+  async execute(empresaId: string, user: User): Promise<void> {
+    if (user.type !== UserType.ADMIN) {
+      throw new NotAllowedError();
+    }
+
+    const empresa = await this.empresaRepository.findById(empresaId);
+
+    if (!empresa) {
+      throw new ResourceNotFoundError('Empresa não encontrada');
+    }
+
+    empresa.status = EmpresaStatus.SUSPENSA;
+    empresa.decidido_por = user;
+    empresa.decidido_em = new Date();
+
+    await this.empresaRepository.save(empresa);
+  }
+}
